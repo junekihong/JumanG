49dcce8b10da27d310ab345067a11eedd85060d6
Juneki Hong <junekihong@gmail.com>
2013-05-02 00:30:53 -0400

    stripped out the underscores when outputting to Tikz. So that we can run ethane.dot

--- TikzWriter.py
+++ TikzWriter.py
@@ -13,14 +13,22 @@ def tikGraph(graph, l = None):
 	output += "\t\\tikzstyle{every node} = [circle, fill=gray!30, minimum size = 2cm]\n"
 
 	for n in graph.getNodeList():
-		output+="\t\\node ("+n.Name+") at ("+"{0:.2f}".format(n.PosX)+", "+"{0:.2f}".format(n.PosY)+") {"+n.Name+"};\n"
+                cleanedName = n.Name
+                cleanedName = cleanedName.replace("_","")
+                
+		output+="\t\\node ("+cleanedName+") at ("+"{0:.2f}".format(n.PosX)+", "+"{0:.2f}".format(n.PosY)+") {"+cleanedName+"};\n"
 
 	for n1 in graph.getNodeList():
 		for (n2, t) in graph.AdjacencyList[n1]:
+                        cleanedN = n1.Name
+                        cleanedN = cleanedN.replace("_", "")
+                        cleanedN2 = n2.Name
+                        cleanedN2 = cleanedN2.replace("_","")
+
 			if t.Type==1:
-				output+="\t\\draw [->] ("+n1.Name+")--("+n2.Name+");\n"
+				output+="\t\\draw [->] ("+cleanedN+")--("+cleanedN2+");\n"
 			else:
-				output+="\t\\draw [-] ("+n1.Name+")--("+n2.Name+");\n"
+				output+="\t\\draw [-] ("+cleanedN+")--("+cleanedN2+");\n"
 
 
 	output += "\\end{tikzpicture}\n"
